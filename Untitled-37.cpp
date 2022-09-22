d18ac538260931b9a0166ae1ada802218598b93b
harshsiinghh <90918177+harshsiinghh@users.noreply.github.com>
2022-09-23 03:17:41 +0530

    Add files via upload

---
+++ Untitled-37.cpp
@@ -0,0 +1,28 @@
+/**VALIDATE A BST
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   bool isValidBST(TreeNode* root, TreeNode* min=NULL, TreeNode* max= NULL){
+    if(root==NULL){
+        return true;
+    }
+    if(min!=NULL && root->val<=min->val){
+        return false;
+    }
+    if(max!=NULL && root->val>=max->val){
+        return false;
+    }
+    bool left= isValidBST(root->left,min, root);
+    bool right = isValidBST(root->right , root , max);
+    return (left && right);
+}
+};
